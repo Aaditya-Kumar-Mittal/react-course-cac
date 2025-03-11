6dca978afb3644d965d4af99619d93144614c9db
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2025-03-11 20:30:08 +0530

    feat: Completed Project for React Router Dom

---
+++ 08understand-react-router-dom/src/components/about_us/AboutUs.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="py-16 bg-white">
+      <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+          <div className="md:5/12 lg:w-5/12">
+            <img
+              src="https://img.freepik.com/free-vector/palm-tree-concept-illustration_114360-9480.jpg?t=st=1741702156~exp=1741705756~hmac=ce39988139b8d66963cbf0a3169f6a07cf23548ca125f2e8b8b19a4b76106da8&w=740"
+              alt="image"
+            />
+          </div>
+          <div className="md:7/12 lg:w-6/12">
+            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+              React development is carried out by passionate developers
+            </h2>
+            <p className="mt-6 text-gray-600">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
+              omnis voluptatem accusantium nemo perspiciatis delectus atque
+              autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
+              consequatur! Officiis id consequatur atque doloremque!
+            </p>
+            <p className="mt-4 text-gray-600">
+              Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
+              expedita at? Asperiores nemo possimus nesciunt dicta veniam
+              aspernatur quam mollitia.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ 08understand-react-router-dom/src/components/index.js
@@ -0,0 +1,18 @@
+import Header from "./headers/Header";
+import Footer from "./footers/Footer";
+import Home from "./home/Home";
+import AboutUs from "./about_us/AboutUs";
+import ContactUs from "./contact_us/ContactUs";
+import User from "./user/User";
+import Github, { gitHubInfoLoader } from "./user/Github";
+
+export {
+  Header,
+  Footer,
+  Home,
+  AboutUs,
+  ContactUs,
+  User,
+  Github,
+  gitHubInfoLoader,
+};
